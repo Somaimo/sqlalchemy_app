20174e6f0f9c636b3bbd589a1fedfc780f4f7842
Somaimo <camao@bluewin.ch>
2020-10-26 21:56:15 +0100

    added shell extension for flask

--- alchemy_app.py
+++ alchemy_app.py
@@ -1,2 +1,7 @@
 # alchemy_app.py
-from app import app
+from app import app, db
+from app.models import CityModel, TeamModel, PlayerModel, User
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'TeamModel': TeamModel, 'CityModel': CityModel, 'PlayerModel': PlayerModel}
