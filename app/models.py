1a200dee8e8707d6980eb6fc34f5a41510729c13
Somaimo <camao@bluewin.ch>
2020-10-28 22:23:38 +0100

    added profile page and extended user model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,13 @@
 # app/models.py
 from sqlalchemy import Column, Integer, String, Text, DateTime, Float, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.ext.declarative import declarative_base
 from app import db
 from datetime import datetime
+from flask_login import UserMixin
+from app import login
+from hashlib import md5
 
 
 class PlayerModel(db.Model):
@@ -87,7 +91,7 @@ class CityModel(db.Model):
         nullable=False
     )
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     """Data Model for users."""
     __tablename__ = "sqlalchemy_app_user"
     id = Column(Integer, primary_key=True)
@@ -98,3 +102,18 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -2,28 +2,52 @@
 import os
 from app import app
 from flask import render_template, flash, redirect, url_for
+from flask import request
+from werkzeug.urls import url_parse
 from app.forms import LoginForm
 #from models import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from os import environ
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 
 # login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    teams = user.team
+    return render_template('user.html', user=user, teams=teams)
 
 # index page
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = { 'username': 'Marc'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -34,5 +58,5 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
     
